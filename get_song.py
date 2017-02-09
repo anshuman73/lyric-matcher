93c2c0f73406e2939e1e2ba0f401d7e259fbbd67
anshuman73 <dev.anshuman73@gmail.com>
2017-02-09 11:21:41 +0530

    make it work ;)

--- get_song.py
+++ get_song.py
@@ -4,21 +4,28 @@
 # Works with Python 2.7
 
 import json
-import urllib
+import requests
 
 def main(query):
 	#get the data
-	raw_data = urllib.urlopen("https://www.musixmatch.com/search/%s/lyrics" % query).readlines()[30]
-	#Data has some HTML, parse it to make it perfect json
-	raw_data = raw_data[raw_data.find('{') : raw_data.rfind('}') + 1]
-	data = json.loads(raw_data) #Ready to query data
-	song =  data['lyricsTracks']['0']['attributes']['track_name']
-	artist = data['lyricsTracks']['0']['attributes']['artist_name']
-	album = data['lyricsTracks']['0']['attributes']['album_name']
-
-	print "\n\nSong Name:", song
-	print "\nArtist:", artist
-	print "\nAlbum:", album
-
+	headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)'
+                             'Chrome/53.0.2785.116 Safari/537.36'}
+	raw_data = requests.get("https://www.musixmatch.com/search/%s/lyrics" % query, headers=headers).text.encode('utf-8')
+	#raw_data is now a HTML dump, parse it to make it perfect json
+	raw_data = raw_data[raw_data.find('{', raw_data.find('mxmProps')) : raw_data.find('</script>', raw_data.find('mxmProps'))]
+	data = json.loads(raw_data) # Data ready to be queried
+	
+	total_results = data['lyricsTracks']['length']
+	
+	print "Top 5 results are:\n"
+	
+	for x in xrange(5):
+		song =  data['lyricsTracks'][str(x)]['attributes']['track_name']
+		artist = data['lyricsTracks'][str(x)]['attributes']['artist_name']
+		album = data['lyricsTracks'][str(x)]['attributes']['album_name']
+		
+		print "\n\nSong Name:", song
+		print "\nArtist:", artist
+		print "\nAlbum:", album
 
 main(str(raw_input("\nEnter the lyrics: \n\n")))
